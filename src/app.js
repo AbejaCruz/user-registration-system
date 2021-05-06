497991c41196dc97359ebc71727237f41eef1f7d
Diego A Cruz <dacruzt@example.com>
2021-05-05 23:48:26 -0500

    create if db conecction and create mysql in xammp

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const mysql = require('mysql');
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodejs_login'
+});
 
+db.connect((error) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log("Mysql Connected...")
+    }
+});
 const app = express();
 
-app.get("/",(req,res) =>{
+app.get("/", (req, res) => {
     res.send("<h1>Home Page</h1>")
 });
 
 app.listen(3000, () => {
     console.log("server started on Port 3000");
 })
- 
